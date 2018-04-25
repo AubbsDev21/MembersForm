65cc1792bb56a28683ca0fd0581d083705ab2498
Aubre <bodyaubre@gmail.com>
2018-04-25 14:46:57 -0400

    Mysql connect work

--- app.js
+++ app.js
@@ -3,8 +3,26 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const mysql = require('mysql')
+
 
 app.use(morgan('short'))
+
+app.get("/user/:id", (req, res) => {
+  console.log("Featching the user by id " + req.params.id)
+
+  // Connecting to the my sql database  
+  const connction = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'memberForm'
+  })
+   //fetching the values in the table then putting them in json format
+  connction.query("SELECT * FROM users", (err, rows, fields) => {
+    console.log("vlaues are being qurey")
+    res.json(rows)
+  })
+})
 //   "/" mean root dir
 app.get("/", (req, res) => {
   console.log("Responding to root route")
