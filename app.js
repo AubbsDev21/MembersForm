e0c1757c65f24e95557a24a54d2e64a45738cd6e
Aubre <bodyaubre@gmail.com>
2018-04-26 09:52:45 -0400

    serving some html file

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const mysql = require('mysql')
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static('./public'))
 
 app.use(morgan('short'))
 
+app.post('/create_user', (req, res) => {
+  console.log("Creating new user")
+  res.end()
+})
 app.get("/user/:id", (req, res) => {
   console.log("Featching the user by id " + req.params.id)
 
